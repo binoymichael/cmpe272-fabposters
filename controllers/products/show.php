5c167babc1a61a57acb0bd785171e12fcae4d253
Binoy Prakash <prakash.binoy@gmail.com>
2016-10-31 14:53:28 -0700

    Removing debug statements from products/show page

--- controllers/products/show.php
+++ controllers/products/show.php
@@ -6,11 +6,10 @@ function capitalised($movie) {
 preg_match('/\d+/', $_SERVER['REQUEST_URI'], $matches);
 $movie_id = $matches[0];
 $movie = Movie::find($database_connection, $movie_id);
-debug($_SERVER);
 
-// if ($movie) {
-// 	require '../views/products/show.view.php';
-// } else {
-// 	require '../controllers/errors/404.php';
-// }
+if ($movie) {
+	require '../views/products/show.view.php';
+} else {
+	require '../controllers/errors/404.php';
+}
 
