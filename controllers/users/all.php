d1b8bfa6a9bea20541232a80b5396f7e16dcd876
Binoy Prakash <prakash.binoy@gmail.com>
2016-11-09 15:48:48 -0800

    Added logic to display user details from external website

--- controllers/users/all.php
+++ controllers/users/all.php
@@ -1,14 +1,16 @@
 <?php
-    // $ch = curl_init("http://localhost:8887/users/list");
-    // $output = curl_exec($ch); 
-    // curl_close($ch);
-    // echo $output;
-	// $json_decoded = json_decode($json_string, true);
-	// foreach ($json_decoded as $record) {
-	// 	echo $record['first_name'] . " " . $record['last_name'] . "<br/>";
-	// }
+	$url = "http://gargoos.com/user_list.txt";  
+	$ch = curl_init();  
+	// set URL and other appropriate options  
+	curl_setopt($ch, CURLOPT_URL, $url);  
+	curl_setopt($ch, CURLOPT_HEADER, 0);  
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+	// grab URL and pass it to the browser  
+	$output = curl_exec($ch);  
+	// close curl resource, and free up system resources  
+	curl_close($ch);  
 
-	// // $json_decoded = json_decode($json_string, true);
-	// // foreach ($json_decoded as $record) {
-	// // 	echo $record['first_name'] . " " . $record['last_name'] . "<br/>";
-	// // }
+	$gargoos_users = json_decode($output, true);
+	$fabposters_users = User::all($database_connection);
+
+	require '../views/users/all.view.php';
---
+++ views/users/all.view.php
@@ -0,0 +1,64 @@
+<?php require '../views/partials/header.php'; ?> 
+<div class="container top-buffer">
+	<div class="col-sm-5">
+		<div class="page-header">
+			<h3>Gargoos Users</h3>
+		</div>
+		<div class="row">
+			<?php if(count($gargoos_users) > 0): ?>
+				<table class="table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						foreach ($gargoos_users as $user) {
+							echo '<tr>';
+								echo "<td>" . $user['firstName'] . ' ' . $user['lastName'] . "</td>";
+								echo "<td>" . $user['email'] . "</td>";
+							echo '</tr>';
+						}
+					?>
+				</tbody>
+				</table>
+
+			<?php else: ?>
+				No users found!
+			<?php endif ?>
+		</div>
+	</div>
+	<div class="col-sm-5 col-sm-offset-1">
+		<div class="page-header">
+			<h3>Fabposters Users</h3>
+		</div>
+		<div class="row">
+			<?php if(count($fabposters_users) > 0): ?>
+				<table class="table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						foreach ($fabposters_users as $user) {
+							echo '<tr>';
+								echo "<td>" . $user['first_name'] . ' ' . $user['last_name'] . "</td>";
+								echo "<td>" . $user['email'] . "</td>";
+							echo '</tr>';
+						}
+					?>
+				</tbody>
+				</table>
+
+			<?php else: ?>
+				No users found!
+			<?php endif ?>
+		</div>
+	</div>
+</div>
+<?php require '../views/partials/footer.php'; ?>
