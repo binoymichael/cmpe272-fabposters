b67d1b427c140ebf9d5e533fb64690efcf66d750
Binoy Prakash <prakash.binoy@gmail.com>
2016-11-11 20:23:18 -0800

    Added airwind users

--- controllers/users/all.php
+++ controllers/users/all.php
@@ -10,7 +10,19 @@
 	// close curl resource, and free up system resources  
 	curl_close($ch);  
 
+	$url2 = "http://airwind.me/userjson.php";  
+	$ch = curl_init();  
+	// set URL and other appropriate options  
+	curl_setopt($ch, CURLOPT_URL, $url2);  
+	curl_setopt($ch, CURLOPT_HEADER, 0);  
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);  
+	// grab URL and pass it to the browser  
+	$output2 = curl_exec($ch);  
+	// close curl resource, and free up system resources  
+	curl_close($ch);  
+
 	$gargoos_users = json_decode($output, true);
+	$airwind_users = json_decode($output2, true);
 	$fabposters_users = User::all($database_connection);
 
 	require '../views/users/all.view.php';
--- views/users/all.view.php
+++ views/users/all.view.php
@@ -1,6 +1,6 @@
 <?php require '../views/partials/header.php'; ?> 
 <div class="container top-buffer">
-	<div class="col-sm-5">
+	<div class="col-sm-3">
 		<div class="page-header">
 			<h3>Gargoos Users</h3>
 		</div>
@@ -30,7 +30,37 @@
 			<?php endif ?>
 		</div>
 	</div>
-	<div class="col-sm-5 col-sm-offset-1">
+	<div class="col-sm-3 col-sm-offset-1">
+		<div class="page-header">
+			<h3>Airwind Users</h3>
+		</div>
+		<div class="row">
+			<?php if(count($airwind_users) > 0): ?>
+				<table class="table">
+				<thead>
+					<tr>
+						<th>Name</th>
+						<th>Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						foreach ($airwind_users as $user) {
+							echo '<tr>';
+								echo "<td>" . $user['first_name'] . ' ' . $user['last_name'] . "</td>";
+								echo "<td>" . $user['email'] . "</td>";
+							echo '</tr>';
+						}
+					?>
+				</tbody>
+				</table>
+
+			<?php else: ?>
+				No users found!
+			<?php endif ?>
+		</div>
+	</div>
+	<div class="col-sm-3 col-sm-offset-1">
 		<div class="page-header">
 			<h3>Fabposters Users</h3>
 		</div>
