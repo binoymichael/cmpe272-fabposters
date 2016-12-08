25ae6f793e229d7ea0760e621e874302ae9e60e5
Binoy Prakash <prakash.binoy@gmail.com>
2016-12-07 19:23:09 -0800

    Added authentication for child api

--- controllers/api/products/update.php
+++ controllers/api/products/update.php
@@ -1,8 +1,26 @@
 <?php
-     $data = json_decode(file_get_contents("php://input"), true);
-     foreach ($data as $movie_id => $quantity) 
-     {
-	    Movie::update($database_connection, $movie_id, $quantity);
-     }
 
-    
+$secret = $app_config['merkato']['seller_secret'];
+
+if (isset(getallheaders()['nonce'])) {
+    $nonce = getallheaders()['nonce'];
+}
+
+if (isset(getallheaders()['Authorization'])) {
+    $digest = getallheaders()['Authorization'];
+}
+
+$body = file_get_contents("php://input");
+$signature = "POST+" . $nonce . "+" . $body;
+$computed_digest = rawurlencode(base64_encode(hash_hmac('sha256', $signature, $secret, true)));
+
+
+if ($digest === $computed_digest) {
+    $data = json_decode($body, true);
+    foreach ($data as $movie_id => $quantity) 
+    {
+        Movie::update($database_connection, $movie_id, $quantity);
+    }
+}
+
+
